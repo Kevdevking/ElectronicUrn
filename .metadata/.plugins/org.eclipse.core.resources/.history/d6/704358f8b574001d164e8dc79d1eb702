6c49ddd5ec1faa8984c76cb8d5b69effd3cd7bf1
Kevin Toledo <kevintoledodev@outlook.com>
2022-12-07 17:06:42 -0300

    first commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/d6/704358f8b574001d164e8dc79d1eb702
@@ -0,0 +1,7 @@
+package assetsEnum;
+
+public enum VotingCapability {
+	Mandatory_vote,
+	Optional_vote,
+	Cant_vote;
+}
---
+++ Urna_Eletronica/src/assets/FinalResult.java
@@ -0,0 +1,43 @@
+package assets;
+
+public class FinalResult {
+	
+	private Politicians president;
+	private Politicians governor;
+	private Politicians senator;
+	private Politicians congressman;
+	private Politicians stateRepresentative;
+	
+	public Politicians getPresident() {
+		return president;
+	}
+	public void setPresindent(Politicians president) {
+		this.president = president;
+	}
+	public Politicians getGovernor() {
+		return governor;
+	}
+	public void setGovernor(Politicians governor) {
+		this.governor = governor;
+	}
+	public Politicians getSenator() {
+		return senator;
+	}
+	public void setSenator(Politicians senator) {
+		this.senator = senator;
+	}
+	public Politicians getCongressman() {
+		return congressman;
+	}
+	public void setCongressman(Politicians congressman) {
+		this.congressman = congressman;
+	}
+	public Politicians getStateRepresentative() {
+		return stateRepresentative;
+	}
+	public void setStateRepresentative(Politicians stateRepresentative) {
+		this.stateRepresentative = stateRepresentative;
+	}
+	
+	
+}
